33ff986d8aae58c3b6a9c168bbd12d2dfa511be1
Your Name <you@example.com>
2023-01-24 16:25:19 +0330

    added file 2

---
+++ arshiapiri-maktab88-part11-hw11/Weather API/weatherApi.js
@@ -0,0 +1,52 @@
+
+
+$(() => {
+  $(".main-content").fadeOut();
+    $.get("https://restcountries.com/v2/all", function (data) {
+      data.forEach((item) => {
+        let countries = `<option>${item.name}</option>`;
+        $("#selector").append(countries);
+      });
+    });
+  
+    $("#selector").change(function () {
+      $.get("https://restcountries.com/v2/all", function (data, status) {
+        data.forEach((element) => {
+          const {name,nativeName,capital,region,population,timezones, flags,languages,callingCodes} = element;
+  
+          if (name === $("#selector").val()) {
+            $("#country").text(name)
+            $("#NativeName").text(nativeName)
+            $("#capital").text(capital)
+            $("#region").text(region)
+            $("#population").text(population)
+
+            $(".mapouter").html(`<iframe width="100%" height="250"
+            src="https://maps.google.com/maps?q=${capital}=&z=13&ie=UTF8&iwloc=&output=embed"
+            frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+        </iframe>`)
+
+            $("#time-zone").text(timezones)
+            $('#flag').css({'background-image':`url(${flags.svg})`,'background-size':'cover'})
+            $("#callingCode").html(`<h2>${callingCodes}</h2>`)
+            $.get(
+              `https://api.openweathermap.org/data/2.5/weather?q=${capital.toString()}&APPID=3ef5728414414623794036f295042ac2`,function(weatherData,status){
+                const {wind:{speed},main:{humidity,temp},visibility} = weatherData
+                $("#windSpeed").text(speed)
+                $("#humidity").text(humidity)
+                $("#temperature").text(Math.ceil(temp-273))
+                $("#visibility").text(visibility)
+              }
+            );
+          }
+        });
+      });
+      $(".main-content").fadeIn();
+    });
+  });
+
+
+
+  $.get("https://restcountries.com/v2/all", function (data, status){
+    console.log(data[1]);
+  })
